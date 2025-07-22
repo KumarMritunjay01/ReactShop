32aef17e3fdc27d85e084265f37f50f33c481d22
kumar <kmritunjay525@gmail.com.com>
2025-07-23 00:07:35 +0530

    deleted some unused component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import CartPage from "./pages/CartPage";
 import ProductDetail from "./pages/ProductDetail";
 import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import LoginPage from "./components/LoginPage";
-import ProductList from "./features/products/ProductList";
 
 function App() {
   return (
@@ -13,11 +11,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<ProductList />} />
         <Route path="/product-detail?/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
