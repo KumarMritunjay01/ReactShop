2e6fc98917f0f2d1863c660e43d77b9e97b43964
kumar <kmritunjay525@gmail.com.com>
2025-07-25 23:36:21 +0530

    add functionality of increment and decrement value of cart item and also calculat the total price of item in the cart

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { removeFromCart } from "../features/cart/cartSlice";
+import {
+  removeFromCart,
+  addTodoCart,
+  increaseItemCount,
+  decreseItemCount,
+} from "../features/cart/cartSlice";
+import { current } from "@reduxjs/toolkit";
 function CartPage() {
   const { cartItem } = useSelector((state) => state.cartItem);
-  const [count, setCount] = useState(1);
   const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    let price = 0;
+    cartItem.map((item) => {
+      price += item.count * Math.floor(item.price);
+    });
+    setTotalPrice(price);
+  }, [current.count, current]);
+  console.log(cartItem);
 
   useEffect(() => {}, [cartItem]);
   const dispatch = useDispatch();
 
-  const removeItemHandler = (id) => {
-    dispatch(removeFromCart(id));
+  const increaseItemHandler = (id) => {
+    dispatch(increaseItemCount(id));
   };
+
+  const decreaseItemHandler = (id) => {
+    dispatch(decreseItemCount(id));
+  };
+
   return (
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">Cart</h2>
@@ -27,11 +46,8 @@ function CartPage() {
       {cartItem.length === 0 && <p>No items...</p>}
 
       {cartItem.map((item) => (
-        <div className="w-full h-full">
-          <div
-            key={item.id}
-            className="w-full flex justify-between min-h-30 items-center "
-          >
+        <div className="w-full h-full" key={item.id}>
+          <div className="w-full flex justify-between min-h-30 items-center ">
             <div className="flex w-1/4 justify-around items-center">
               <img src={item.image} alt={item.title} className="max-w-20" />
               <h3 className="text-lg font-semibold text-center mb-2">
@@ -39,17 +55,29 @@ function CartPage() {
               </h3>
             </div>
             <div className="w-1/4 items-center justify-center flex">
-              <b>Price:</b> ${item.price}
+              <b>Price: </b> $ {Math.floor(item.price)}
             </div>
             <div className="w-1/4 items-center justify-center flex">
               <div className="flex w-1/4 h-8 justify-around border-1 border-black items-center rounded-sm">
-                <button className="text-3xl cursor-pointer">-</button>
-                <div>{count}</div>
-                <button className="text-xl cursor-pointer">+</button>
+                <button
+                  className="text-3xl cursor-pointer"
+                  onClick={() => decreaseItemHandler(item.id)}
+                  disabled={item.count === 1}
+                >
+                  -
+                </button>
+                <div>{item.count}</div>
+                <button
+                  className="text-xl cursor-pointer"
+                  onClick={() => increaseItemHandler(item.id)}
+                  disabled={item.count === 5}
+                >
+                  +
+                </button>
               </div>
             </div>
             <div className="w-1/4 items-center justify-center flex">
-              ${totalPrice}
+              $ {Math.floor(Math.floor(item.price) * item.count)}
             </div>
             <hr />
           </div>
@@ -63,10 +91,12 @@ function CartPage() {
           <hr />
           <div className="h-2/3 flex justify-between items-center text-xl ml-4 mr-4">
             <h3 className="w-1/2">Sub total</h3>
-            <p className="flex w-1/2 justify-center">$ {totalPrice}</p>
+            <p className="flex w-1/2 justify-center">
+              $ {Math.floor(totalPrice)}
+            </p>
           </div>
 
-          <div className="bg-black text-white text-lg font-semibold w-full h-12 rounded-lg flex justify-center items-center ">
+          <div className="bg-black text-white text-lg font-semibold w-full h-12 rounded-lg flex justify-center items-center cursor-pointer">
             Proceed to Checkout
           </div>
         </div>
