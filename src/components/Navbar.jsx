aeb10360ca7c7e574e77dbe9c69b58c446e811dd
kumar <kmritunjay525@gmail.com.com>
2025-07-26 21:45:39 +0530

    updating style of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
       <div className="sm:ml-20 w-full sm:w-3/5 flex justify-center sm:justify-start items-center text-2xl sm:text-3xl font-semibold mb-3 sm:mb-0">
         <Link
           to="/"
-          className="border-2 border-white px-3 py-1 rounded hover:bg-white hover:text-blue-950 transition duration-200"
+          className="border-1 border-gray-500 px-3 py-1 rounded light:hover:bg-white light:hover:text-blue-950 transition duration-200 dark:hover:text-3xl"
         >
           ReactShop
         </Link>
@@ -18,13 +18,13 @@ function Navbar() {
       <div className="w-full sm:w-2/5 flex justify-center sm:justify-end items-center gap-4 text-base sm:text-xl font-medium text-black">
         <Link
           to="/"
-          className="bg-white border border-white px-4 py-1 rounded hover:bg-blue-100 transition duration-200 text-center w-24"
+          className="border-1 border-gray-500 text-white  px-4 py-1 rounded  transition duration-200 text-center w-24 dark:hover:text-2xl mr-2"
         >
           Home
         </Link>
         <Link
           to="/cart"
-          className="bg-white border border-white px-4 py-1 rounded hover:bg-blue-100 transition duration-200 text-center w-24"
+          className="border-1 border-gray-500 text-white mr-3 px-4 py-1 rounded light:hover:bg-blue-100 transition duration-200 text-center w-24 dark:hover:text-2xl"
         >
           Cart
         </Link>
