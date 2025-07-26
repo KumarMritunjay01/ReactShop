ca9900427da8aece1757115e8b78724b9034ec29
kumar <kmritunjay525@gmail.com.com>
2025-07-26 21:46:46 +0530

    change style of seachbar and filter section

--- src/components/SearchAndFilter.jsx
+++ src/components/SearchAndFilter.jsx
@@ -7,16 +7,16 @@ function SearchAndFilter({
   setSelectCategory,
 }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full px-4 py-4 bg-white dark:bg-gray-900 text-black dark:text-white rounded-md shadow">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-full px-4 py-4 bg-white dark:text-white rounded-md shadow">
       <input
         type="text"
         placeholder="Search Products With Name..."
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
-        className="w-full h-10 px-4 py-2 border border-black dark:border-white rounded-md bg-white dark:bg-gray-800 text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full h-10 px-4 py-2 border border-black  dark:border-white rounded-md bg-white dark:bg-gray-800 text-black dark:text-white placeholder-gray-500 dark:placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:scale-102"
       />
       <select
-        className="w-full h-10 px-4 py-2 border border-black dark:border-white rounded-md bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+        className="w-full h-10 px-4 py-2 border border-black dark:border-white rounded-md bg-white dark:bg-gray-800 text-black dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 dark:hover:scale-102 cursor-pointer"
         value={selectCategory}
         onChange={(e) => setSelectCategory(e.target.value)}
       >
